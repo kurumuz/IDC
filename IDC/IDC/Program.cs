ce5a86694ee783cdca702200ced338a963f62954
Eren <kurumuz1@gmail.com>
2018-06-04 20:19:42 +0300

    initial

---
+++ IDC/IDC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IDC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
